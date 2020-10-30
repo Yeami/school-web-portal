ca806300a876210c4eb908245915abe8b2b63b87
Yeami <ivanov.nikita@yeami.group>
2020-10-30 14:52:16 +0200

    Implement the database connection config

---
+++ server/db/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('[INFO] MongoDB was connected successfully'))
+  .catch(err => {
+    console.log(`[INFO] MongoDB connection error: ${err.message}`);
+  });

5cc4c223fbbfd628a698742b9bb0aead6fbeb82d
Yeami <ivanov.nikita@yeami.group>
2021-01-04 01:58:44 +0200

    Implement the auth system on the router component and make some refactoring

--- client/src/components/RouterComponent.js
+++ client/src/components/RouterComponent.js
@@ -8,7 +8,7 @@ import {
   RocketTwoTone
 } from '@ant-design/icons';
 
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,7 +16,17 @@ import {
   Link,
 } from 'react-router-dom';
 
+import history from '../utils/history';
+
 import LoginComponent from './LoginComponent';
+import {useSelector} from 'react-redux';
+import {isEmpty} from '../utils/utils';
+import ProfileView from '../views/ProfileView';
+import GuardedRoute from '../utils/route-guard';
+import NewsView from '../views/NewsView';
+import ClassesView from '../views/ClassesView';
+import SubjectsView from '../views/SubjectsView';
+import TeachersView from '../views/TeachersView';
 
 const routerWrapper = {
   display: 'flex',
@@ -37,9 +47,26 @@ const svgStyles = {
   color: 'red',
 };
 
-export default function RouterComponent() {
+function RouterComponent() {
+  const user = useSelector(state => state.userReducer.user);
+  const token = localStorage.getItem('token');
+  const isEmptyUser = isEmpty(user) && user.constructor === Object;
+
+  console.log('User: ', user);
+  console.log('Token: ', token);
+
+  if (isEmptyUser && !token) {
+    console.log('[INFO] User info and token are empty');
+  } else if (isEmptyUser && token) {
+    console.log('[INFO] User info is empty, but token exists');
+  } else {
+    console.log('[INFO] User info and token exists');
+  }
+
+  const isAuthenticated = (!isEmptyUser && Boolean(token));
+
   return (
-    <Router>
+    <Router history={history}>
       <div style={routerWrapper}>
         <div className="logo">
           <RocketTwoTone style={svgStyles} twoToneColor="red"/> School Web Portal
@@ -50,9 +77,12 @@ export default function RouterComponent() {
             <Link to="/news">News</Link>
           </Menu.Item>
 
-          <Menu.Item key="classes" icon={<TeamOutlined style={svgStyles}/>}>
-            <Link to="/classes">Classes</Link>
-          </Menu.Item>
+          {
+            isAuthenticated &&
+            <Menu.Item key="classes" icon={<TeamOutlined style={svgStyles}/>}>
+              <Link to="/classes">Classes</Link>
+            </Menu.Item>
+          }
 
           <Menu.Item key="subjects" icon={<ReadOutlined style={svgStyles}/>}>
             <Link to="/subjects">Subjects</Link>
@@ -61,6 +91,13 @@ export default function RouterComponent() {
           <Menu.Item key="teachers" icon={<HighlightOutlined style={svgStyles}/>}>
             <Link to="/teachers">Teachers</Link>
           </Menu.Item>
+
+          {
+            isAuthenticated &&
+            <Menu.Item key="profile" icon={<HighlightOutlined style={svgStyles}/>}>
+              <Link to="/profile">Profile</Link>
+            </Menu.Item>
+          }
         </Menu>
 
         <div style={{fontSize: '1.15rem'}}>
@@ -69,22 +106,15 @@ export default function RouterComponent() {
       </div>
 
       <Switch>
-        <Route exact path="/login">
-          <LoginComponent/>
-        </Route>
-        <Route path="/news">
-          <h1>News</h1>
-        </Route>
-        <Route path="/classes">
-          <h1>Classes</h1>
-        </Route>
-        <Route path="/subjects">
-          <h1>Subjects</h1>
-        </Route>
-        <Route path="/teachers">
-          <h1>Teachers</h1>
-        </Route>
+        <Route exact path='/login' component={LoginComponent}/>
+        <Route path='/news' component={NewsView}/>
+        <GuardedRoute path='/classes' component={ClassesView} auth={isAuthenticated}/>
+        <Route path='/subjects' component={SubjectsView}/>
+        <Route path='/teachers' component={TeachersView}/>
+        <GuardedRoute path='/profile' component={ProfileView} auth={isAuthenticated}/>
       </Switch>
     </Router>
   );
-};
+}
+
+export default RouterComponent;

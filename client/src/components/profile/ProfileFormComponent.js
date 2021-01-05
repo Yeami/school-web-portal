51e0838ff10c37e8feef97e6408d58e8c0053637
Yeami <ivanov.nikita@yeami.group>
2021-01-05 03:48:58 +0200

    Implement the profile form component

---
+++ client/src/components/profile/ProfileFormComponent.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {Button, Form, Input} from 'antd';
+
+const ProfileFormComponent = ({ onChange, fields }) => {
+  return (
+    <Form
+      layout="vertical"
+      name="profile-form"
+      fields={fields}
+      onFieldsChange={(_, allFields) => {
+        onChange(allFields);
+      }}
+    >
+      <Form.Item
+        label="First name:"
+        name="firstName"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your first name!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Last name:"
+        name="lastName"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your last name!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Patronymic:"
+        name="patronymic"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your patronymic!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Email:"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your email!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Update
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default ProfileFormComponent;

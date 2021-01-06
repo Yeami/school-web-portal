bd7731cae50e34cd4cef27b1f0d27c4e70f56bcf
Yeami <ivanov.nikita@yeami.group>
2021-01-07 01:44:29 +0200

    Implement the publication router with two end-points, get all publications and create a new one

---
+++ server/routers/publication.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Publication = require('../models/publication');
+const auth = require('../middleware/auth');
+
+const router = express.Router();
+
+router.get('/all', async (req, res) => {
+  try {
+    res.status(200).send(Publication.find().populate('author'));
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post('/new', auth, async (req, res) => {
+  try {
+    const author = req.user._id;
+    const { title, content, imageUrl } = req.body.publication;
+
+    res.status(201).send(new Publication({
+      title,
+      content,
+      imageUrl,
+      author,
+    }).save());
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = router;

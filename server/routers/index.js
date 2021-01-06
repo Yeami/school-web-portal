1a2e4efdc99314e415b1dbe5010ab256a53945ae
Yeami <ivanov.nikita@yeami.group>
2021-01-07 01:43:50 +0200

    Implement the new route related to the publications

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,5 +1,7 @@
 const userRoute = require('./user');
+const publicationRoute = require('./publication');
 
 module.exports = (app) => {
   app.use('/users', userRoute);
+  app.use('/publications', publicationRoute);
 };

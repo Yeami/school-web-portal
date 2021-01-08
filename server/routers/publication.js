13653443f402247a9ce18a850a2daf7d985f94f9
Yeami <ivanov.nikita@yeami.group>
2021-01-08 03:46:15 +0200

    Make some minor update on the get all publications end-point

--- server/routers/publication.js
+++ server/routers/publication.js
@@ -6,7 +6,12 @@ const router = express.Router();
 
 router.get('/all', async (req, res) => {
   try {
-    res.status(200).send(Publication.find().populate('author'));
+    res.status(200).send(
+      await Publication
+        .find()
+        .populate('author')
+        .then((p) => p),
+    );
   } catch (error) {
     res.status(400).send(error);
   }

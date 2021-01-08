6a9b50353e754ec74ad4a5faeec75c3b3b8cdd9b
Yeami <ivanov.nikita@yeami.group>
2021-01-08 03:50:45 +0200

    Add the publication reducer to the root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 
 import userReducer from './userReducer';
+import publicationReducer from './publicationReducer';
 
 const rootReducer = combineReducers({
   userReducer,
+  publicationReducer,
 });
 
 export default rootReducer;

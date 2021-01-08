a463dd59f2443f5c923e647aa43b94e5789814c2
Yeami <ivanov.nikita@yeami.group>
2021-01-08 03:48:43 +0200

    Implement the get all publications action

--- client/src/actions/publicationActions.js
+++ client/src/actions/publicationActions.js
@@ -1,6 +1,22 @@
 import axios from 'axios';
 import {notify} from '../utils/notification';
 
+const setPublications = (payload) => ({type: 'SET_PUBLICATIONS', payload});
+
+export const getPublications = () => dispatch => {
+  axios.get(`http://localhost:3100/publications/all`, {
+    headers: {
+      'Authorization': localStorage.getItem('token'),
+    }
+  })
+    .then(res => {
+      dispatch(setPublications(res.data));
+    })
+    .catch(() => {
+      notify('error', 'Error', 'Sorry, something went wrong and we can`t load the list of all publications. Please, try one more time later.');
+    });
+};
+
 export const createPublication = (publication) => dispatch => {
   axios.post(`http://localhost:3100/publications/new`, {publication}, {
     headers: {

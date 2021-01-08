015e507119585221bcd14fa3539e98d3306106db
Yeami <ivanov.nikita@yeami.group>
2021-01-09 00:08:17 +0200

    Implement the basic teachers view

--- client/src/views/TeachersView.js
+++ client/src/views/TeachersView.js
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {getAllUsers} from '../store/actions/userActions';
+import {Button} from 'antd';
+import {PlusOutlined} from '@ant-design/icons';
+import {pageWrapper, titleCardWrapper} from '../utils/styles';
+import TeacherCardComponent from "../components/teacher/TeacherCardComponent";
 
-function TeachersView() {
+const contentWrapper = {
+  display: 'flex',
+  flexWrap: 'wrap',
+}
+
+function TeachersView({isAuth}) {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(getAllUsers()), [dispatch]);
+
+  const teachers = useSelector(state => state.userReducer.users);
+
+  // const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    // setVisible(true);
+  };
+
+  // const onClose = () => {
+  //   setVisible(false);
+  // };
+
   return (
-    <h1>Teachers page</h1>
+    <div style={pageWrapper}>
+      <div style={titleCardWrapper}>
+        <h1>Teachers page</h1>
+        {
+          isAuth ?
+            <Button
+              type="dashed"
+              style={{margin: '1rem 0'}}
+              icon={<PlusOutlined/>}
+              onClick={showDrawer}
+            >
+              Add new teacher
+            </Button>
+            : null
+        }
+        {/*<NewPublicationDrawerComponent*/}
+        {/*  onClose={onClose}*/}
+        {/*  visible={visible}*/}
+        {/*/>*/}
+      </div>
+
+      <div style={contentWrapper}>
+        {
+          teachers && teachers.length > 0 ?
+            teachers.map((p, i) => <TeacherCardComponent {...p} key={i}/>)
+            : null
+        }
+      </div>
+
+    </div>
   );
 }
 

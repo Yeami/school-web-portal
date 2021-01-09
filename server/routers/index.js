f23507073536db520b245d0333b434a57ebb17ff
Yeami <ivanov.nikita@yeami.group>
2021-01-09 03:54:11 +0200

    Add the positions route to the list of all routes

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,9 +1,11 @@
 const userRoute = require('./user');
 const publicationRoute = require('./publication');
 const teacherRoute = require('./teacher');
+const positionRoute = require('./position');
 
 module.exports = (app) => {
   app.use('/users', userRoute);
   app.use('/publications', publicationRoute);
   app.use('/teachers', teacherRoute);
+  app.use('/positions', positionRoute);
 };

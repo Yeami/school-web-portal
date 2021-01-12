bfcd237d1c070fcde2e503ffabcd616c46ae1b10
Yeami <ivanov.nikita@yeami.group>
2021-01-12 22:45:04 +0200

    Add the subject route to the list of all routes

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,10 +2,12 @@ const userRoute = require('./user');
 const publicationRoute = require('./publication');
 const teacherRoute = require('./teacher');
 const positionRoute = require('./position');
+const subjectRoute = require('./subject');
 
 module.exports = (app) => {
   app.use('/users', userRoute);
   app.use('/publications', publicationRoute);
   app.use('/teachers', teacherRoute);
   app.use('/positions', positionRoute);
+  app.use('/subjects', subjectRoute);
 };

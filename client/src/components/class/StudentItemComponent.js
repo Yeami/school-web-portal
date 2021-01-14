c01cad52e3b65794892b50c5b422ca35c2f0b35c
Yeami <ivanov.nikita@yeami.group>
2021-01-14 02:36:52 +0200

    Implement the student item component

---
+++ client/src/components/class/StudentItemComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Avatar, List} from 'antd';
+import {CloseOutlined} from '@ant-design/icons';
+
+const StudentItemComponent = ({students}) => {
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={students}
+      renderItem={(student, i) => (
+        <List.Item actions={[
+          <a><CloseOutlined/></a>
+        ]}>
+          <List.Item.Meta
+            avatar={<Avatar src={student.photoUrl}/>}
+            title={`${i + 1}. ${student.firstName} ${student.lastName} ${student.patronymic}`}
+            description={student.email}
+          />
+        </List.Item>
+      )}
+    />
+  )
+};
+
+export default StudentItemComponent;

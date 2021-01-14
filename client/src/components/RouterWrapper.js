bb81aae61b3f27efc905143d375057ab908f0151
Yeami <ivanov.nikita@yeami.group>
2021-01-14 03:47:23 +0200

    Rename the route auth prop name

--- client/src/components/RouterWrapper.js
+++ client/src/components/RouterWrapper.js
@@ -26,10 +26,10 @@ function RouterWrapper(props) {
       <Switch>
         <Route exact path='/login' render={(props) => (<LoginView {...props} isAuth={isAuthenticated}/>)}/>
         <Route path='/news' render={(props) => (<PublicationsView {...props} isAuth={isAuthenticated}/>)}/>
-        <GuardedRoute path='/classes' component={ClassesView} auth={isAuthenticated}/>
+        <GuardedRoute path='/classes' component={ClassesView} isAuth={isAuthenticated}/>
         <Route path='/subjects' render={(props) => (<SubjectsView {...props} isAuth={isAuthenticated}/>)}/>
         <Route path='/teachers' render={(props) => (<TeachersView {...props} isAuth={isAuthenticated}/>)}/>
-        <GuardedRoute path='/profile' component={ProfileView} auth={isAuthenticated}/>
+        <GuardedRoute path='/profile' component={ProfileView} isAuth={isAuthenticated}/>
       </Switch>
     </Router>
   );
--- client/src/utils/route-guard.js
+++ client/src/utils/route-guard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const GuardedRoute = ({ component: Component, auth, ...rest }) => {
-  console.log('[AUTH] Is user authenticated: ' + auth);
+const GuardedRoute = ({ component: Component, isAuth, ...rest }) => {
+  console.log('[AUTH] Is user authenticated: ' + isAuth);
   return(
-    <Route {...rest} render={(props) => (auth === true ? <Component {...props} /> : <Redirect to='/news' />)} />
+    <Route {...rest} render={(props) => (isAuth === true ? <Component {...props} /> : <Redirect to='/news' />)} />
   )
 }
 

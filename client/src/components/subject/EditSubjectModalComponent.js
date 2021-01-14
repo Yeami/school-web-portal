e3cb8b76cc9839a08e293d29936829079b6a7337
Yeami <ivanov.nikita@yeami.group>
2021-01-14 20:19:33 +0200

    Implement the edit subject modal window component

---
+++ client/src/components/subject/EditSubjectModalComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Input, Modal} from 'antd';
+
+const {TextArea} = Input;
+
+const EditSubjectModalComponent = (
+  {
+    subject, isVisible, handleOk, handleCancel, onNameChange, onDescriptionChange
+  }
+) => {
+  return (
+    <Modal
+      title="Update the subject info"
+      visible={isVisible}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <Input
+        defaultValue={subject.name}
+        name="name"
+        placeholder="The subject name"
+        onChange={onNameChange}
+      />
+      <TextArea
+        defaultValue={subject.description}
+        name="description"
+        placeholder="The subject description"
+        onChange={onDescriptionChange}
+        rows={4}
+      />
+    </Modal>
+  )
+};
+
+export default EditSubjectModalComponent;
